8e5dc3dfa65c88f2a23f7c27bfba4ded1af4ae5d
Sumit <sumityadav0376@gmail.com>
2021-02-18 17:39:11 +0530

    Create a view for home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,12 @@
 module.exports.home = function(req, res){
-    return res.end('<h1>Express is up for Codeial!</h1>')
+    // Before settingup view engine
+    // return res.end('<h1>Express is up for Codeial!</h1>');
+
+    // After settingup view engine
+    return res.render('home', {
+        title: "Home"
+    });
+
 }
 
 // module.exports.actionName = function(req, res){}
