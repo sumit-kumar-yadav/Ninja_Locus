1bbb7b8b68bdd15714d0e257f2d3d6bfd054b9bd
Sumit <sumityadav0376@gmail.com>
2021-02-18 16:49:18 +0530

    Create few routes and controllers

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,10 @@ console.log('router loaded');
 
 
 router.get('/', homeController.home);
+router.use('/users', require('./users'));
+
+// for any further routes, access from here
+// router.use('/routerName', require('./routerfile));
+
 
 module.exports = router;
