dab7ad62b9146b43a2488bb5e38d2e3275cfe564
Sumit <sumityadav0376@gmail.com>
2021-02-25 21:12:54 +0530

    Linking our MongoDB using Mongoose

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 800;
 const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
 
 //Setting static files
 app.use(express.static('./assets'));
