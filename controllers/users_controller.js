9303b24a129c0deecd0faebdcaa5e9c92fa4a652
Sumit <sumityadav0376@gmail.com>
2021-03-01 13:34:25 +0530

    User Sign Up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
+const User = require('../models/user');
+
+
 module.exports.profile = function(req, res){
     return res.render('user_profile', {
         title: 'User Profile'
@@ -22,8 +25,29 @@ module.exports.signIn = function(req, res){
 
 // get the sign up data
 module.exports.create = function(req, res){
-    // TODO later
+    if (req.body.password != req.body.confirm_password){
+        console.log('Password is incorrect');
+        return res.redirect('back');
+    }
+
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('error in finding user in signing up'); return}
+
+        console.log(user, typeof(user));
+        if (!user){
+            User.create(req.body, function(err, user){
+                if(err){console.log('error in creating user while signing up'); return}
+
+                return res.redirect('/users/sign-in');
+            })
+        }else{
+            console.log('User is already Signed Up');
+            return res.redirect('back');
+        }
+
+    });
 }
+
 
 // sign in and create a session for the user
 module.exports.createSession = function(req, res){
--- routes/users.js
+++ routes/users.js
@@ -8,5 +8,7 @@ router.get('/profile', usersController.profile);
 router.get('/sign-up', usersController.signUp);
 router.get('/sign-in', usersController.signIn);
 
+router.post('/create', usersController.create);
+
 
 module.exports = router;
