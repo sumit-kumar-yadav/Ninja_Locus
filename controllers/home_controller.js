82a27babe1fa247b161c55fddf7d713060d36afa
Sumit <sumityadav0376@gmail.com>
2021-03-07 02:09:55 +0530

    Display Posts and related User, Check Authentication on Creating Post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,23 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res){
-    console.log(req.cookies);
-    res.cookie('user_id', 25);
-    res.cookie('new_id', '500');
-    return res.render('home', {
-        title: "Home"
-    });
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
+
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: "Codeial | Home",
+    //         posts:  posts
+    //     });
+    // });
+
+    // populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts:  posts
+        });
+    })
 
 }
 
