77574cad068e97736c03ff6ab214c0a763a1d6f0
Sumit <sumityadav0376@gmail.com>
2021-03-09 22:17:43 +0530

    User profile links

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,16 +1,7 @@
 const Post = require('../models/post');
+const User = require('../models/user');
 
 module.exports.home = function(req, res){
-    // console.log(req.cookies);
-    // res.cookie('user_id', 25);
-
-    // Post.find({}, function(err, posts){
-    //     return res.render('home', {
-    //         title: "Codeial | Home",
-    //         posts:  posts
-    //     });
-    // });
-
     // populate the user of each post
     Post.find({})
     .populate('user')
@@ -21,10 +12,14 @@ module.exports.home = function(req, res){
         }
     })
     .exec(function(err, posts){
-        return res.render('home', {
-            title: "Codeial | Home",
-            posts:  posts
+        User.find({}, function(err, users){
+            return res.render('home', {
+                title: "Codeial | Home",
+                posts:  posts,
+                all_users: users
+            });
         });
+
     })
 
 }
