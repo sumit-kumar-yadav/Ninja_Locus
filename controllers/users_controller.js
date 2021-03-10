5572cedd0abcf96c8f6f215a2b1789ad254eabc6
Sumit <sumityadav0376@gmail.com>
2021-03-10 23:18:16 +0530

    Updating a User Profile

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,6 +1,6 @@
 const User = require('../models/user');
 
-
+// Render the profile
 module.exports.profile = function(req, res){
     User.findById(req.params.id, function(err, user){
         return res.render('user_profile', {
@@ -10,6 +10,18 @@ module.exports.profile = function(req, res){
     });
 
 }
+
+// Action for updating the profile
+module.exports.update = function(req, res){
+    if(req.user.id == req.params.id){
+        User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+            return res.redirect('back');
+        });
+    }else{
+        return res.status(401).send('Unauthorized');
+    }
+}
+
 
 
 // render the sign up page
