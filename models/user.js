677969c41a6238910277f3ec03938132113877ad
Sumit <sumityadav0376@gmail.com>
2021-03-17 00:05:09 +0530

    Installing multer, Configuring multer for User Avatar

--- models/user.js
+++ models/user.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 
+const multer = require('multer');
+const path = require('path');
+const AVATAR_PATH = path.join('/uploads/users/avatars');
+
+
+
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -13,10 +19,29 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
+    },
+    avatar: {
+        type: String
     }
 }, {
     timestamps: true
 });
+
+
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {  // cb is callback function
+      cb(null, path.join(__dirname, '..', AVATAR_PATH)); // To set the destination of the file in local disc storage
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now());  // Date.now() method returns the number of milliseconds since January 1, 1970 00:00:00 UTC.
+      // It's used to differentiate the files with same name set by the uploader users to prevent overwriting the existing file
+    }
+})
+
+
+// static methods
+userSchema.statics.uploadedAvatar = multer({storage: storage}).single('avatar');
+userSchema.statics.avatarPath = AVATAR_PATH;
 
 
 const User = mongoose.model('User', userSchema);
