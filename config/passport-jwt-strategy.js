bd1a89a1f8291c0bca137ace8249631226cc20da
Sumit <sumityadav0376@gmail.com>
2021-03-20 02:07:11 +0530

    Created a Token

--- config/passport-jwt-strategy.js
+++ config/passport-jwt-strategy.js
@@ -7,7 +7,7 @@ const User = require('../models/user');
 
 let opts = {
     jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken,  // To extract the token
-    secretOrKey: 'codeial'               // To encript the token
+    secretOrKey: 'codeial'               // To decript the token
 }
 
 
