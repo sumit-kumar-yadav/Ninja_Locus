835d22b79de21ee8f8e144002453cfbea71cd87d
Sumit <sumityadav0376@gmail.com>
2021-04-09 15:32:27 +0530

    Environment Variables

--- config/environment.js
+++ config/environment.js
@@ -21,7 +21,26 @@ const development = {
 }
 
 const production = {
-    name: 'production'
+    name: 'production',
+    asset_path: '/' + process.env.CODEIAL_ASSET_PATH,
+    // asset_path: process.env.CODEIAL_ASSET_PATH  ->> Saved value as /assets & run server in powershell instead of in git bash of vs code -->> Works fine
+    session_cookie_key: process.env.CODEIAL_SESSION_COOKIE_KEY,
+    db: process.env.CODEIAL_DB,
+    smtp: {
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,  // for TLS
+        secure: false,
+        auth: {
+            user: process.env.CODEIAL_GMAIL_USERNAME,
+            pass: process.env.CODEIAL_GMAIL_PASSWORD
+        }
+    },
+    google_client_id: process.env.CODEIAL_GOOGLE_CLIENT_ID, 
+    google_client_secret: process.env.CODEIAL_GOOGLE_CLIENT_SECRET, 
+    google_call_back_url: process.env.CODEIAL_GOOGLE_CALLBACK_URL,   // Authorized redirect URI as filled in google credential of this project
+    jwt_secret: process.env.CODEIAL_JWT_SECRET
 }
 
-module.exports = development;
+// module.exports = development;
+module.exports = eval(process.env.CODEIAL_ENVIRONMENT) == undefined ? development : eval(process.env.CODEIAL_ENVIRONMENT);
