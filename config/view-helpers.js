f663f918db3824f7a399f988f3324f85d261fb3a
Sumit <sumityadav0376@gmail.com>
2021-04-23 23:06:54 +0530

    Correct filepath of view-helpers

--- config/view-helpers.js
+++ config/view-helpers.js
@@ -8,7 +8,7 @@ module.exports = (app) => {
     // Put assetPath function in the locals of views via app
     app.locals.assetPath = function(filePath){
         if (env.name == 'development'){
-            return '/' + filePath;  // i.e fetch assets from assets folder which are not renamed
+            return filePath;  // i.e fetch assets from assets folder which are not renamed
         }
 
         return '/' + JSON.parse(fs.readFileSync(path.join(__dirname, '../public/assets/rev-manifest.json')))[filePath];
